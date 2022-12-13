44e93c7ce91b32286d8b205983f0858e730b1849
Harshjeet.Kumar <Harshjeet.Kumar@valtech.com>
2022-12-13 17:10:10 +0530

    test

---
+++ RestaurantManagement/RestaurantAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantBLL.Services;
+using RestaurantEntity;
+using System;
+using System.Collections.Generic;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeService _employeeService;
+
+        public EmployeeController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("GetEmployees")]//
+        public IEnumerable<Employee> GetEmployees()
+        {
+             #region Function for getting  the Employee
+
+            return _employeeService.GetEmployee();
+            #endregion
+        }
+
+
+
+        [HttpDelete("DeleteEmployee")]
+        public IActionResult DeleteEmployee(int employeeId)
+        {
+            #region Function for deleting the employee by its employeeId.
+            try
+            {
+                _employeeService.DeleteEmployee(employeeId);
+                return Ok("Employee deleted Successfully");
+            }
+            catch
+            {
+                return BadRequest(400);
+            }
+            #endregion
+        }
+
+        [HttpPut("UpdateEmployee")]
+        public IActionResult UpdateEmployee([FromBody] Employee employee)
+        {
+            #region Function for updating  the employee its object
+            try
+            {
+                _employeeService.UpdateEmployee(employee);
+                return Ok("Employee Updated Successfully");
+            }
+            catch
+            {
+                return BadRequest(400);
+            }
+            #endregion
+        }
+
+        [HttpGet("GetEmployeeById")]
+        public Employee GetEmployeeById(int employeeId)
+        {
+            #region Function for getting it all details of the   the employee by  its employeeId
+
+            return _employeeService.GetEmployeeById(employeeId);
+
+            #endregion
+        }
+
+        [HttpPost("AddEmployee")]
+        public IActionResult AddEmployee([FromBody] Employee employeeInfo)
+        {
+            #region Function for updating  the employee  by its object
+            try
+            {
+                _employeeService.AddEmployee(employeeInfo);
+                return Ok("Register successfully!!");
+            }
+            catch
+            {
+                return BadRequest(400);
+            }
+            #endregion
+        }
+        [HttpPost("Login")]
+        public Employee Login([FromBody] Employee employeeInfo)
+        {
+            #region Function of login
+            try
+            {
+                Employee Employee = _employeeService.Login(employeeInfo);
+
+                return Employee;
+            }
+            catch
+            {
+
+                return null;
+            }
+            #endregion
+        }
+    }
+}
---
+++ RestaurantManagement/RestaurantAPI/Controllers/FoodController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantBLL.Services;
+using RestaurantEntity;
+using System.Collections.Generic;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private FoodService _foodService;
+
+        public FoodController(FoodService foodService)
+        {
+            _foodService = foodService;
+        }
+
+        [HttpGet("GetFoods")]//
+        public IEnumerable<Food> GetFoods()
+        {
+            #region Function for geeting the Food 
+
+            return _foodService.GetFood();
+            #endregion
+        }
+
+
+
+        [HttpDelete("DeleteFood")]
+        public IActionResult DeleteFood(int foodId)
+
+        {
+            #region  Function for deleting the Food by its foodId.
+            try
+            {
+                _foodService.DeleteFood(foodId);
+                return Ok("Food deleted Successfully");
+            }
+            catch
+            {
+                return BadRequest(400);
+            }
+            #endregion
+        }
+
+        [HttpPut("UpdateFood")]
+        public IActionResult UpdateFood([FromBody] Food food)
+        {
+            #region Function for Updating the Food by its object
+            try
+            {
+                _foodService.UpdateFood(food);
+                return Ok("Food Updated Successfully");
+            }
+            catch
+            {
+                return BadRequest(400);
+            }
+            #endregion
+        }
+
+        [HttpGet("GetFoodById")]
+        public Food GetFoodById(int foodId)
+        {
+            #region Function for geeting the Food by its foodId
+
+            return _foodService.GetFoodById(foodId);
+            #endregion
+        }
+
+        [HttpPost("AddFood")]
+        public IActionResult AddFood([FromBody] Food foodInfo)
+        {
+            #region Function for Adding the Food by its foodId
+            try
+            {
+                _foodService.AddFood(foodInfo);
+                return Ok("Register successfully!!");
+            }
+            catch
+            {
+                return BadRequest(400);
+            }
+
+            #endregion
+        }
+
+    }
+}
